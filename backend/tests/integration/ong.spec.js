977407c74766b690a48e697c0001273cf4592c73
MaykonM <maykon.marcolino@gmail.com>
2020-04-01 15:32:46 -0300

    pasta do backend

---
+++ backend/tests/integration/ong.spec.js
@@ -0,0 +1,35 @@
+const request = require('supertest');
+const app = require('../../src/app');
+const connection = require('../../src/database/connection');
+
+describe('ONG', () => {
+    beforeEach(async () => {
+        await connection.migrate.rollback();
+        await connection.migrate.latest();
+    });
+
+    afterAll(async () => {
+        await connection.destroy();
+    });
+
+    it('should be able to create a new ONG', async () => {
+        const response = await request(app)
+        .post('/ongs')
+        .send({
+            name: "APAD",
+	        email: "contato@teste.com",
+	        whatsapp: "11000000000",
+	        city: "São Paulo",
+	        uf: "SP"
+        });
+
+        expect(response.body).toHaveProperty('id');
+        expect(response.body.id).toHaveLength(8);
+    });
+
+    it('should be able to search a new case', async () => {
+        const response = await request(app)
+        .get('/profile')
+        .set('Authorization', '4354d1cb');
+    });
+});
